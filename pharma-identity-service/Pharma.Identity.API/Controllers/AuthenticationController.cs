9c409d47e35733147a38c0bebd3329e6fdb4c7c9
longtk5 <trinhkimlong811@gmail.com>
2026-02-02 20:36:20 +0700

    remove redundant api

--- pharma-identity-service/Pharma.Identity.API/Controllers/AuthenticationController.cs
+++ pharma-identity-service/Pharma.Identity.API/Controllers/AuthenticationController.cs
@@ -21,25 +21,6 @@ public class AuthenticationController(IMediator mediator) : ControllerBase
         return result.ToResult();
     }
 
-    [HttpPost("resend-email-otp")]
-    public async Task<IResult> ResendEmailOtpAsync([FromBody] ResendEmailOtpRequest request, CancellationToken cancellationToken)
-    {
-        var result = await mediator.Send(new ResendEmailOtp.ResendEmailOtpCommand(request.Email), cancellationToken);
-
-        return result.ToResult();
-    }
-
-    [HttpPost("validate-email-otp")]
-    public async Task<IResult> ValidateEmailOtpAsync([FromBody] ValidateOtpRequest request, CancellationToken cancellationToken)
-    {
-        var result = await mediator.Send(
-            new ValidateEmailOtp.ValidateEmailOtpCommand(request.Email, request.Otp),
-            cancellationToken
-        );
-
-        return result.ToResult();
-    }
-
     [HttpPost("login")]
     public async Task<IResult> LoginAsync([FromBody] LoginRequest request, CancellationToken cancellationToken)
     {
