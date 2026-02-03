240cc37237c42ca1689084fd62deb44d9b5fab27
longtk5 <trinhkimlong811@gmail.com>
2026-02-03 20:03:53 +0700

    remove unnecessary line break

--- pharma-identity-service/Pharma.Identity.Application/Features/Authentication/DTOs/LoginResponse.cs
+++ pharma-identity-service/Pharma.Identity.Application/Features/Authentication/DTOs/LoginResponse.cs
@@ -1,4 +1,3 @@
 namespace Pharma.Identity.Application.Features.Authentication.DTOs;
 
-public record LoginResponse(string? AccessToken, string? RefreshToken, string? UserRole
-);
+public record LoginResponse(string? AccessToken, string? RefreshToken, string? UserRole);
